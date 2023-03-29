1a50e9fa8eee0b20e0726d5f21ff7113f13f56f5
qewaru <124196857+qewaru@users.noreply.github.com>
2023-03-29 16:49:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mysql = require('mysql2');
 const stripe = require('stripe')('sk_test_51MhsuUKRgv2CaMZJgM7NxJAscPwWUtbMwdaS7Yy3wYTYI8lBWnkVlbGCF90Co8FPqXRJlIu35AXIF9v6ZxGglp6Z00zKFZekTx');
 const nodemailer = require('nodemailer')
 
+const port = 8000
+
 const app = express();
 
 app.use(session({
@@ -29,13 +31,13 @@ const transporter = nodemailer.createTransport({
   }
 })
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: '17aezakmi',
-  database: 'shopbaseorders'
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   port: 3306,
+//   password: '17aezakmi',
+//   database: 'shopbaseorders'
+// });
 
 var globalEmail = ''
 var globalOrderId = null
@@ -57,26 +59,26 @@ app.post('/submit', async (req, res) => {
   const order_id = Math.floor(Math.random() * 9000) + 1000;
   globalOrderId = order_id
   
-  const customerSql = 'INSERT INTO customers (order_id, user_name, user_surname, user_phone, user_email) VALUES (?, ?, ?, ?, ?)';
-  connection.query(customerSql, [order_id, name, surname, phone, mail], async (error, customerResults, fields) => {
-    if (error) throw error;
-    console.log(`Customer data saved to database`);
+//   const customerSql = 'INSERT INTO customers (order_id, user_name, user_surname, user_phone, user_email) VALUES (?, ?, ?, ?, ?)';
+//   connection.query(customerSql, [order_id, name, surname, phone, mail], async (error, customerResults, fields) => {
+//     if (error) throw error;
+//     console.log(`Customer data saved to database`);
 
-  const orderSql = 'INSERT INTO orders (order_id, item_name, item_quantity, item_size) VALUES (?, ?, ?, ?)';
-    for (let item of cartArray) {
-      connection.query(orderSql, [order_id, item.name, item.quantity, item.size], (error, orderResults, fields) => {
-        if (error) throw error;
-        console.log(`Item ${item.name} saved to database`);
-      });
-    }
+//   const orderSql = 'INSERT INTO orders (order_id, item_name, item_quantity, item_size) VALUES (?, ?, ?, ?)';
+//     for (let item of cartArray) {
+//       connection.query(orderSql, [order_id, item.name, item.quantity, item.size], (error, orderResults, fields) => {
+//         if (error) throw error;
+//         console.log(`Item ${item.name} saved to database`);
+//       });
+//     }
 
-  const deliverySql = 'INSERT INTO delivery (order_id, country, city, postcode, street, a_comment) VALUES (?, ?, ?, ?, ?, ?)'
-  connection.query(deliverySql, [order_id, country, city, postcode, street, comment], async (error, deliveryResults, fields) => {
-    if (error) throw error;
-    console.log(`Delivery data saved to database`);
-  })
+//   const deliverySql = 'INSERT INTO delivery (order_id, country, city, postcode, street, a_comment) VALUES (?, ?, ?, ?, ?, ?)'
+//   connection.query(deliverySql, [order_id, country, city, postcode, street, comment], async (error, deliveryResults, fields) => {
+//     if (error) throw error;
+//     console.log(`Delivery data saved to database`);
+//   })
   
-  console.log(`Name: ${name}, Surname: ${surname}, Phone: ${phone}, Email: ${mail}, Array: ${JSON.stringify(cartArray)}`);
+//   console.log(`Name: ${name}, Surname: ${surname}, Phone: ${phone}, Email: ${mail}, Array: ${JSON.stringify(cartArray)}`);
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -135,6 +137,6 @@ You can expect your order in a couple of days.`
   res.sendStatus(200);
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server started on port 8000');
 });

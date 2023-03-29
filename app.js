339c890edf4cdf32293abea92659053392e30d92
qewaru <124196857+qewaru@users.noreply.github.com>
2023-03-29 16:06:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const session = require('express-session')
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const stripe = require('stripe')('sk_test_51MhsuUKRgv2CaMZJgM7NxJAscPwWUtbMwdaS7Yy3wYTYI8lBWnkVlbGCF90Co8FPqXRJlIu35AXIF9v6ZxGglp6Z00zKFZekTx');
+const nodemailer = require('nodemailer')
 
 const app = express();
 
+app.use(session({
+  secret: 'sk_test_51MhsuUKRgv2CaMZJgM7NxJAscPwWUtbMwdaS7Yy3wYTYI8lBWnkVlbGCF90Co8FPqXRJlIu35AXIF9v6ZxGglp6Z00zKFZekTx',
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
 app.use(express.static('html'));
 app.use("/style", express.static('style'));
 app.use("/images", express.static('images'));
 app.use("/scripts", express.static('scripts'));
 
-const testItems = new Map ([
-  [1, {priceInCents: 10000, name: "Item1"}],
-  [2, {priceInCents: 50000, name: "Item2"}],
-  [3, {priceInCents: 20000, name: "Item3"}]
-])
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'ivory.yundt@ethereal.email',
+        pass: 'GYmWfY8xeHgmh2KgMX'
+  }
+})
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -25,8 +37,15 @@ const connection = mysql.createConnection({
   database: 'shopbaseorders'
 });
 
+var globalEmail = ''
+var globalOrderId = null
+
 app.post('/submit', async (req, res) => {
-  const { name, surname, phone, email, itemsArray, country, city, postcode, street, comment} = req.body;
+  const { name, surname, phone, mail, itemsArray, country, city, postcode, street, comment} = req.body;
+  globalEmail = req.body.mail
+  globalName = req.body.name
+  globalAdress = req.body.street + ', ' + req.body.city
+
   let cartArray;
   try {
     cartArray = JSON.parse(itemsArray);
@@ -35,9 +54,11 @@ app.post('/submit', async (req, res) => {
     return res.status(400).send('Invalid JSON in itemsArray parameter');
   }
 
-  const order_id = Math.floor(Math.random() * 900) + 100;;
+  const order_id = Math.floor(Math.random() * 9000) + 1000;
+  globalOrderId = order_id
+  
   const customerSql = 'INSERT INTO customers (order_id, user_name, user_surname, user_phone, user_email) VALUES (?, ?, ?, ?, ?)';
-  connection.query(customerSql, [order_id, name, surname, phone, email], async (error, customerResults, fields) => {
+  connection.query(customerSql, [order_id, name, surname, phone, mail], async (error, customerResults, fields) => {
     if (error) throw error;
     console.log(`Customer data saved to database`);
 
@@ -55,7 +76,7 @@ app.post('/submit', async (req, res) => {
     console.log(`Delivery data saved to database`);
   })
   
-  console.log(`Name: ${name}, Surname: ${surname}, Phone: ${phone}, Array: ${JSON.stringify(cartArray)}`);
+  console.log(`Name: ${name}, Surname: ${surname}, Phone: ${phone}, Email: ${mail}, Array: ${JSON.stringify(cartArray)}`);
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -78,6 +99,40 @@ app.post('/submit', async (req, res) => {
 
     res.redirect(303, session.url);
   });
+});
+
+app.post("/success", async (req, res) => {
+  const event = req.body;
+
+  var emailTitle = 
+  `BEBXSHOP Order #${globalOrderId}`
+  var emailText =
+  `Hello, ${globalName}!
+We have recieved your order #${globalOrderId}.
+Shipping destination is - ${globalAdress}.
+You can expect your order in a couple of days.`
+
+  if (event.type === 'payment_intent.succeeded') {
+    // const successPaymentSql = 'INSERT INTO orders (payment) VALUES ()'
+    // connection.query(successPaymentSql )
+
+    const mailSuccess = {
+      from: 'ivory.yundt@ethereal.email',
+      to: globalEmail,
+      subject: emailTitle,
+      text: emailText
+    }
+
+    transporter.sendMail(mailSuccess, (error, info) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Email sent")
+      }
+    })
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(8000, () => {

1a3c3bfa05b0eaba2b3557040834f65a067a66e1
qewaru <124196857+qewaru@users.noreply.github.com>
2023-03-30 13:32:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ const port = 8000
 
 const app = express();
 
-app.use(session({
-  secret: 'sk_test_51MhsuUKRgv2CaMZJgM7NxJAscPwWUtbMwdaS7Yy3wYTYI8lBWnkVlbGCF90Co8FPqXRJlIu35AXIF9v6ZxGglp6Z00zKFZekTx',
-  resave: false,
-  saveUninitialized: true
-}));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(express.static('html'));
@@ -31,13 +25,13 @@ const transporter = nodemailer.createTransport({
   }
 })
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: '17aezakmi',
-  database: 'shopbaseorders'
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   port: 3306,
+//   password: '17aezakmi',
+//   database: 'shopbaseorders'
+// });
 
 var globalEmail = ''
 var globalOrderId = null
@@ -59,24 +53,24 @@ app.post('/submit', async (req, res) => {
   const order_id = Math.floor(Math.random() * 9000) + 1000;
   globalOrderId = order_id
   
-  const customerSql = 'INSERT INTO customers (order_id, user_name, user_surname, user_phone, user_email) VALUES (?, ?, ?, ?, ?)';
-  connection.query(customerSql, [order_id, name, surname, phone, mail], async (error, customerResults, fields) => {
-    if (error) throw error;
-    console.log(`Customer data saved to database`);
+//   const customerSql = 'INSERT INTO customers (order_id, user_name, user_surname, user_phone, user_email) VALUES (?, ?, ?, ?, ?)';
+//   connection.query(customerSql, [order_id, name, surname, phone, mail], async (error, customerResults, fields) => {
+//     if (error) throw error;
+//     console.log(`Customer data saved to database`);
 
-  const orderSql = 'INSERT INTO orders (order_id, item_name, item_quantity, item_size) VALUES (?, ?, ?, ?)';
-    for (let item of cartArray) {
-      connection.query(orderSql, [order_id, item.name, item.quantity, item.size], (error, orderResults, fields) => {
-        if (error) throw error;
-        console.log(`Item ${item.name} saved to database`);
-      });
-    }
+//   const orderSql = 'INSERT INTO orders (order_id, item_name, item_quantity, item_size) VALUES (?, ?, ?, ?)';
+//     for (let item of cartArray) {
+//       connection.query(orderSql, [order_id, item.name, item.quantity, item.size], (error, orderResults, fields) => {
+//         if (error) throw error;
+//         console.log(`Item ${item.name} saved to database`);
+//       });
+//     }
 
-  const deliverySql = 'INSERT INTO delivery (order_id, country, city, postcode, street, a_comment) VALUES (?, ?, ?, ?, ?, ?)'
-  connection.query(deliverySql, [order_id, country, city, postcode, street, comment], async (error, deliveryResults, fields) => {
-    if (error) throw error;
-    console.log(`Delivery data saved to database`);
-  })
+//   const deliverySql = 'INSERT INTO delivery (order_id, country, city, postcode, street, a_comment) VALUES (?, ?, ?, ?, ?, ?)'
+//   connection.query(deliverySql, [order_id, country, city, postcode, street, comment], async (error, deliveryResults, fields) => {
+//     if (error) throw error;
+//     console.log(`Delivery data saved to database`);
+//   })
   
   console.log(`Name: ${name}, Surname: ${surname}, Phone: ${phone}, Email: ${mail}, Array: ${JSON.stringify(cartArray)}`);
 
@@ -101,7 +95,7 @@ app.post('/submit', async (req, res) => {
 
     res.redirect(303, session.url);
   });
-});
+// });
 
 app.post("/success", async (req, res) => {
   const event = req.body;

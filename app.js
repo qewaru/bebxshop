85bc414e183e48fa834c4a3277bd876105e0fe86
qewaru <124196857+qewaru@users.noreply.github.com>
2023-03-30 19:16:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 8000
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.static('html'));
+app.use("/style", express.static('style'));
+app.use("/images", express.static('images'));
+app.use("/scripts", express.static('scripts'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/html/index.html');
+});
+
+app.post('/submit', async (req, res) => {
+  const itemsArray = req.body.itemsArray
+  let cartArray;
+  try {
+    cartArray = JSON.parse(itemsArray);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send('Invalid JSON in itemsArray parameter');
+  }
+  console.log(cartArray)
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log('Server started on port 8000');
+});
